96ce31210165706b371286a74457bb1a4550ef77
hanwenzhu <thomas.zhu.sh@hotmail.com>
2020-04-21 13:27:39 +0800

    use thumbnails

---
+++ create_thumbnails.py
@@ -0,0 +1,19 @@
+import glob
+import os
+import sys
+
+from PIL import Image
+
+
+width = int(sys.argv[1]) if len(sys.argv) == 2 else 768
+
+for infile in glob.glob('photos/*/*.*'):
+    im = Image.open(infile)
+    im.thumbnail((width, width))
+    outpath = os.path.join(
+        'photos/thumbnails',
+        # 1 for the first 'photos' and -1 for removing file name
+        *infile.split(os.path.sep)[1:-1]
+    )
+    os.makedirs(outpath, exist_ok=True)
+    im.save(os.path.join(outpath, os.path.split(infile)[1]))
