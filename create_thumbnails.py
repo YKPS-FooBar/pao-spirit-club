d983fb6f15be7d6891c7fb10318a1192e76b9475
hanwenzhu <thomas.zhu.sh@hotmail.com>
2020-04-21 14:19:56 +0800

    1. optimize thumbnail creation 2. in case there's no thumbnail yet

--- create_thumbnails.py
+++ create_thumbnails.py
@@ -8,12 +8,14 @@ from PIL import Image
 width = int(sys.argv[1]) if len(sys.argv) == 2 else 768
 
 for infile in glob.glob('photos/*/*.*'):
-    im = Image.open(infile)
-    im.thumbnail((width, width))
     outpath = os.path.join(
         'photos/thumbnails',
         # 1 for the first 'photos' and -1 for removing file name
         *infile.split(os.path.sep)[1:-1]
     )
-    os.makedirs(outpath, exist_ok=True)
-    im.save(os.path.join(outpath, os.path.split(infile)[1]))
+    outfile = os.path.join(outpath, os.path.split(infile)[1])
+    if not os.path.isfile(outfile):
+        os.makedirs(outpath, exist_ok=True)
+        im = Image.open(infile)
+        im.thumbnail((width, width))
+        im.save(outfile)
